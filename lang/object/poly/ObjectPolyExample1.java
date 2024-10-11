982696b9a7ccdf7de5412d0525d858ecf28f589c
narumi <sychoi2008@naver.com>
2024-10-12 00:57:10 +0900

    Object

---
+++ lang/object/poly/ObjectPolyExample1.java
@@ -0,0 +1,25 @@
+package lang.object.poly;
+
+public class ObjectPolyExample1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+
+
+    }
+
+    private static void action(Object obj) {
+        //obj.sound(); // 컴파일 오류
+        //obj.move();
+
+        // 객체에 맞는 다운 캐스팅이 필요
+        if(obj instanceof Dog dog) {
+            dog.sound();
+        } else if(obj instanceof Car car) {
+            car.move();
+        }
+    }
+}
